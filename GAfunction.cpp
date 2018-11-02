9cc989d725595f11a4e6b64c4b1b95c9466888ac
dongpilYu <yoodongphil@naver.com>
2018-11-02 12:58:41 +0900

    Modify main function

--- GAfunction.cpp
+++ GAfunction.cpp
@@ -1,31 +1,62 @@
 // GAfunction.cpp : Defines the entry point for the console application.
-/* 
+/*
 ������ RGA�� extended box crossover, gaussian mutation �����Ѵ�.
 ������ GA���� ����ü�� ���յ��� ���� �� ����� ���´�.
 ���յ��� ���ϴ� �Լ��� ���̽� ���� �ٸ���.
 */
 #include "GeneticAlgorithm.h"
-#include <string>
-const int crossover_rate = 50;
-const int extension_rate = 5;
-// For extended-box Crossover, alpha is extension rate
-const int mutation_rate = 2;
-// For Gaussian mutation, sd is fixed to (upper - lower) / 10
-const int population_size = 20;
-const int number_iterations = 20;
-const int chromosome_size = 20;
-const int tournament_size = population_size / 10;
+#include <iostream>
+using namespace std;
+enum PROBLEM
+{
+    RealEncoding = 0,
+    BinEncoding = 1,
+};
 const int precision = 6;
-const int epoch = 50;
-const std::string path = "C:\\Users\\dongpilYu\\Desktop\\vscode\\RealCodedGA\\best.txt";
-
+const int epoch = 10;
+const std::string path = "C:\\Users\\dong\\Desktop\\VScode\\Real-Encoding-GA\\Log.txt";
+Constraint typeOfProblem(const int &problem_type);
 int main(int argc, char **argv)
 {
-    GeneticAlgorithm ga;
+    enum PROBLEM pr = (PROBLEM)atoi(argv[1]);
+    const int chromosome_size = atoi(argv[2]);
+    const int population_size = atoi(argv[3]);
+    const int tournament_size = population_size / 10;
+    const int number_iterations = atoi(argv[4]);
+    const int problem_type = atoi(argv[5]);
+    const int crossover_rate = atoi(argv[6]);
+    const int extension_rate = 50;           // For extended-box Crossover
+    const int mutation_rate = atoi(argv[7]); // For Gaussian mutation, SD is fixed to (upper - lower) / 10
+    const int royal_number = atoi(argv[8]);
+    const int num_k = atoi(argv[9]);
 
-    Constraint constraint = Constraint::Rastrigin;
-    ga.Initialize(crossover_rate, extension_rate, mutation_rate, population_size, number_iterations, chromosome_size, tournament_size, precision, epoch, path, constraint);
+    GeneticAlgorithm ga;
+    Constraint constraint = typeOfProblem(problem_type);
+
+    ga.Initialize(pr, royal_number, num_k, crossover_rate, mutation_rate, population_size, number_iterations, chromosome_size, tournament_size, precision, epoch, path, constraint);
     ga.Run();
 
     return 0;
+}
+Constraint typeOfProblem(const int &problem_type)
+{
+    switch (problem_type)
+    {
+    case 1:
+        return Constraint::Rosenbrock;
+    case 2:
+        return Constraint::Sphere;
+    case 3:
+        return Constraint::Schwefel;
+    case 4:
+        return Constraint::Rastrigin;
+    case 5:
+        return Constraint::Onemax;
+    case 6:
+        return Constraint::Royalroad;
+    case 7:
+        return Constraint::NKlandscape;
+    case 8:
+        return Constraint::Deceptive;
+    }
 }
