ce27f33fbe26fd7841283d024256569e51e9931c
dongpilYu <yoodongphil@naver.com>
2018-11-19 13:22:53 +0900

    Modify makeFiles.sh

--- makeFiles.sh
+++ makeFiles.sh
@@ -1,1 +1,8 @@
-./GAfunction.exe 0 5 300 300 1 50 2 -1 -1
+
+for i in 4 5 6 7 8 20 25 50 100
+do
+    ./GAfunction.exe 0 $i 300 300 1 50 2 -1 -1 rosenbrock$i
+    ./GAfunction.exe 0 $i 300 300 1 50 2 -1 -1 sphere$i
+    ./GAfunction.exe 0 $i 300 300 1 50 2 -1 -1 schwefel$i
+    ./GAfunction.exe 0 $i 300 300 1 50 2 -1 -1 rastrigin$i
+done
