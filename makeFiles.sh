0df0e5dd6e07718f99b01f93de62d4790a24d999
dongpilYu <yoodongphil@naver.com>
2018-11-21 01:31:44 +0900

    Modify makeFiles.sh

--- makeFiles.sh
+++ makeFiles.sh
@@ -1,8 +1,8 @@
 
 for i in 4 5 6 7 8 20 25 50 100
 do
-    ./GAfunction.exe 0 $i 300 300 1 50 2 -1 -1 rosenbrock$i
-    ./GAfunction.exe 0 $i 300 300 2 50 2 -1 -1 sphere$i
-    ./GAfunction.exe 0 $i 300 300 3 50 2 -1 -1 schwefel$i
-    ./GAfunction.exe 0 $i 300 300 4 50 2 -1 -1 rastrigin$i
+    ./GAfunction.exe 0 $i 300 100 1 50 2 -1 -1 rosenbrock$i.txt
+    ./GAfunction.exe 0 $i 300 100 2 50 2 -1 -1 sphere$i.txt
+    ./GAfunction.exe 0 $i 300 100 3 50 2 -1 -1 schwefel$i.txt
+    ./GAfunction.exe 0 $i 300 100 4 50 2 -1 -1 rastrigin$i.txt
 done
