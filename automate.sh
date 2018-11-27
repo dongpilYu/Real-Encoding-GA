7b96f8a3f3fb7ea80326a59039ec0c9fe7b7937a
dongpilYu <yoodongphil@naver.com>
2018-11-27 09:56:18 +0900

    Modify automate.sh

--- automate.sh
+++ automate.sh
@@ -1,19 +1,19 @@
-for i in 4 5 6 7 8 20 25 50 100 
+for i in 4 5 6 7 8  
 do
     for j in {1..50}
     do
-    	./GAfunction.exe 0 $i 300 300 1 50 2 -1 -1 rosenbrock$i.txt
+    	./GAfunction.exe 0 $i 300 100 1 50 2 -1 -1 rosenbrock$i.txt
     done
     for j in {1..50}
     do
-    	./GAfunction.exe 0 $i 300 300 2 50 2 -1 -1 sphere$i.txt
+    	./GAfunction.exe 0 $i 300 100 2 50 2 -1 -1 sphere$i.txt
     done
     for j in {1..50}
     do
-    	./GAfunction.exe 0 $i 300 300 3 50 2 -1 -1 schwefel$i.txt
+    	./GAfunction.exe 0 $i 300 100 3 50 2 -1 -1 schwefel$i.txt
     done
     for j in {1..50}
     do
-    	./GAfunction.exe 0 $i 300 300 4 50 2 -1 -1 rastrigin$i.txt
+    	./GAfunction.exe 0 $i 300 100 4 50 2 -1 -1 rastrigin$i.txt
     done
 done
